8dd297c8c2cc0135f3d3b24736a756cb1993e3da
MacStudent <macstudent@128-iMAC21.local>
2020-01-21 21:09:44 -0500

    Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,13 @@
+package com.lambton;
+
+public class FirstExample
+{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("manish");
+
+            System.out.println("abc");
+        }
+    }
+}
